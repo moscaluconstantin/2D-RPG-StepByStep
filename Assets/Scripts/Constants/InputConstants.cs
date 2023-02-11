f7421c9e0df093c5b658916c4209f1c14fa19f7c
moscaluconstantin <55128671+moscaluconstantin@users.noreply.github.com>
2023-02-11 14:34:25 +0200

    Add player.
    - Add sprites;
    - Add PlayerMovement.

---
+++ Assets/Scripts/Constants/InputConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Assets.Scripts.Constants
+{
+    public static class InputConstants
+    {
+        public const string AXIS_HORIZONTAL = "Horizontal";
+        public const string AXIS_VERTICAL = "Vertical";
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using Assets.Scripts.Constants;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [Header("Settings")]
+        [SerializeField, Range(0, 100)] private float _movementSpeed = 2;
+
+        [Header("Components")]
+        [SerializeField] private Rigidbody2D _rigidbody;
+
+        private Vector2 _newPosition;
+
+
+
+        private void Update()
+        {
+            var input = new Vector2(Input.GetAxisRaw(InputConstants.AXIS_HORIZONTAL), Input.GetAxisRaw(InputConstants.AXIS_VERTICAL));
+            _newPosition = (Vector2)transform.position + input * (_movementSpeed * Time.deltaTime);
+        }
+
+        private void FixedUpdate()
+        {
+            _rigidbody.MovePosition(_newPosition);
+        }
+    }
+}
