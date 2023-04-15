06a07caa633bb0eff2d388725fa7282abd08f558
moscaluconstantin <55128671+moscaluconstantin@users.noreply.github.com>
2023-04-15 18:53:34 +0300

    Add procedural animations.

---
+++ Assets/Scripts/Infrastructure/ProceduralAnimations.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Infrastructure
+{
+    public static class ProceduralAnimations
+    {
+        public static IEnumerator Move(Transform target, Vector3 from, Vector3 to, float duration, AnimationCurve curve)
+        {
+            float time = 0;
+
+            while (time < duration)
+            {
+                float t = time / duration;
+                float curveT = curve.Evaluate(t);
+
+                target.position = Vector3.Lerp(from, to, curveT);
+
+                time += Time.deltaTime;
+
+                yield return null;
+            }
+
+            target.position = to;
+        }
+
+        public static IEnumerator Rotate(Transform target, Quaternion from, Quaternion to, float duration, AnimationCurve curve)
+        {
+            float time = 0;
+
+            while (time < duration)
+            {
+                float t = time / duration;
+                float curveT = curve.Evaluate(t);
+
+                target.rotation = Quaternion.Lerp(from, to, curveT);
+
+                time += Time.deltaTime;
+
+                yield return null;
+            }
+
+            target.rotation = to;
+        }
+
+        public static IEnumerator Scale(Transform target, Vector3 from, Vector3 to, float duration, AnimationCurve curve)
+        {
+            float time = 0;
+
+            while (time < duration)
+            {
+                float t = time / duration;
+                float curveT = curve.Evaluate(t);
+
+                target.localScale = Vector3.Lerp(from, to, curveT);
+
+                time += Time.deltaTime;
+
+                yield return null;
+            }
+
+            target.localScale = to;
+        }
+    }
+}
---
+++ Assets/Scripts/Precudural Animations/AnimationsManager.cs
@@ -0,0 +1,78 @@
+﻿using Assets.Scripts.Infrastructure;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Precudural_Animations
+{
+    public class AnimationsManager : MonoBehaviour
+    {
+        [Header("Settings")]
+        [SerializeField] private float _duration = 2;
+        [SerializeField] private Vector3[] _endPositions;
+        [SerializeField] private Vector3 _endEulearRotation;
+        [SerializeField] private Vector3[] _endScales;
+
+        [Header("Animation Curves")]
+        [SerializeField] private AnimationCurve[] _positionCurves;
+
+        [Header("Objects")]
+        [SerializeField] private Transform[] _squares;
+
+        private Vector3[] _startPositions;
+
+        private Quaternion _startRotation;
+        private Quaternion _endRotation;
+
+        private bool _isAnimating = false;
+        private bool _initialState = true;
+
+        private void Awake()
+        {
+            _startPositions = new Vector3[_squares.Length];
+
+            for (int i = 0; i < _squares.Length; i++)
+            {
+                _startPositions[i] = _squares[i].position;
+            }
+
+            _startRotation = Quaternion.identity;
+            _endRotation = Quaternion.Euler(_endEulearRotation);
+        }
+
+        private void Update()
+        {
+            if (_isAnimating)
+                return;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _isAnimating = true;
+
+                for (int i = 0; i < _squares.Length; i++)
+                {
+                    if (_initialState)
+                    {
+                        StartCoroutine(ProceduralAnimations.Move(_squares[i], _startPositions[i], _endPositions[i], _duration, _positionCurves[i]));
+                        StartCoroutine(ProceduralAnimations.Rotate(_squares[i], _startRotation, _endRotation, _duration, _positionCurves[i]));
+                        StartCoroutine(ProceduralAnimations.Scale(_squares[i], Vector3.one, _endScales[i], _duration, _positionCurves[i]));
+                    }
+                    else
+                    {
+                        StartCoroutine(ProceduralAnimations.Move(_squares[i], _endPositions[i], _startPositions[i], _duration, _positionCurves[i]));
+                        StartCoroutine(ProceduralAnimations.Rotate(_squares[i], _endRotation, _startRotation, _duration, _positionCurves[i]));
+                        StartCoroutine(ProceduralAnimations.Scale(_squares[i], _endScales[i], Vector3.one, _duration, _positionCurves[i]));
+                    }
+                }
+
+                _initialState = !_initialState;
+                StartCoroutine(Wait(_duration));
+            }
+        }
+
+        private IEnumerator Wait(float second)
+        {
+            yield return new WaitForSeconds(second);
+            _isAnimating = false;
+        }
+    }
+}
